bf0ab357a031c716b50b672d0fc69e238adba781
skibitsky <gleb@skibitsky.com>
2023-05-28 21:16:25 +0900

    Update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -26,11 +26,11 @@ export default function Home() {
          style={{ backgroundImage: `url(/pano.jpg)` }}>
 
       <div className="flex flex-col gap-3 sm:gap-5 backdrop-blur-xl bg-white/75 p-8 lg:p-12 rounded-lg shadow-lg w-4/5 sm:w-3/5">
-        <div className={`${garamond.variable} font-sans text-2xl sm:text-4xl lg:text-6xl font-semibold text-gray-800`}>Gleb Skibitksy</div>
+        <div className={`${garamond.variable} font-sans text-3xl sm:text-4xl lg:text-6xl font-semibold text-gray-800`}>Gleb Skibitsky</div>
         <div className="space-y-3 lg:text-xl">
           <p>{getGreeting()}, I am traveller and software engineer from 🇱🇹 Lithuania.</p>
           <p>
-            Today I am in <span className="bg-amber-100 px-2 py-1 rounded-xl">🇯🇵⛩️ Tokyo, Japan</span>, you can follow me on <a className="text-[#E4405F] font-medium hover:underline" href="https://www.instagram.com/gleb.skibitsky/" target="_blank" rel="noreferrer">Instagram</a> or <a className="text-[#1DA1F2] font-medium hover:underline" href="https://twitter.com/skibitsky" target="_blank" rel="noreferrer">Twitter</a> for updates.
+            Today I am in <span className="text-base bg-amber-100 px-2 py-1 rounded-xl">🇯🇵⛩️ Tokyo, Japan</span>, you can follow me on <a className="text-[#E4405F] font-medium hover:underline" href="https://www.instagram.com/gleb.skibitsky/" target="_blank" rel="noreferrer">Instagram</a> or <a className="text-[#1DA1F2] font-medium hover:underline" href="https://twitter.com/skibitsky" target="_blank" rel="noreferrer">Twitter</a> for updates.
           </p>
           <p>
             This website is currently undergoing 👷‍♂️ improvements. I’m working on new features and content. Stay tuned for updates, and thank you for your patience 🙇‍♂️
